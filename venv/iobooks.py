a3905340b8c882db2d81a7f47bfb2f5b00c471c3
Adam Moszczynski <adam.1.moszczynski@nokia.com>
2021-07-29 09:52:05 +0200

    4.10.naprawianie bledow

--- venv/iobooks.py
+++ venv/iobooks.py
@@ -25,31 +25,37 @@ class IObooks:
         return books
 
     def save_book(self, book):
-        title = book["Title"]
+        title = book["title"]
         title = title.lower()
         title = title.replace(" ", '_')
         with open(self.get_filename(title) + '.json', 'w') as f:
             json.dump(book, f)
 
-def build_book(title, authors, sub_title= '', publisher = '', pubish_year = -1, read_date = '', owner = '',
-               user = '', rating = -1, notes = ''):
-    authors = authors.split(',')
-    d = {
-        "Title": title,
-        "SubTitle": sub_title,
-        "Authors": authors,
-        "Publisher": publisher,
-        "Year": pubish_year,
-        "ReadDate": read_date,
-        "Owner": owner,
-        "User": user,
-        "Rating": rating,
-        "Notes": rating
-    }
-    return d
+def build_book(**kwargs):
+    required = ['title', 'authors']
+    optional = [
+        ("sub_title", ''),
+        ("publisher", ''),
+        ("pubish_year", -1),
+        ("read_date", ''),
+        ("owner", ''),
+        ('user', ''),
+        ('rating', -1),
+        ('notes', '')
+    ]
+
+    for arg in required:
+        if arg not in kwargs:
+            raise Exception(arg + ' nie instnieje w obiekcie!')
+
+    for arg, default in optional:
+        if arg not in kwargs:
+            kwargs[arg] = default
+
+    return kwargs
 
 if __name__ == '__name__':
     books = IObooks('books')
-    book1 = build_book("Wlamywacz", 'Lona, Webber')
+    book1 = build_book(title= "Wlamywacz", authors=['Lona', 'Webber'])
     print(book1)
     books.save_book(book1)
--- venv/organizer.py
+++ venv/organizer.py
@@ -6,15 +6,35 @@ class BooksOrganizer:
         self.io = iobooks.IObooks(dirpath)
         self.books = self.io.load_books()
 
-    def add_book(self,title, authors, *args, **kwargs):
-        book = iobooks.build_book(title, authors, *args,  **kwargs)
+    def add_book(self, **kwargs):
+        book = iobooks.build_book(**kwargs)
         self.io.save_book(book)
 
-    def modify_book(self):
-        pass
+    def filter_book(self,arg, value):
+        l = []
+        for book in self.books:
+            if book[arg] == value:
+                l.append(book)
+        return l
+
+    def find_book(self, arg, value):
+        l = self.filter_book(arg, value)
+        if len(l) != 1:
+            raise Exception('Blad wyszukiwania. Istnieje wiecej niz jedna szukana ksiazka')
+        return l[0]
+
+    def modify_book(self, arg, value, set_arg, set_value):
+        book = self.find_book(arg, value)
+        book[set_arg] = set_value
+        self.io.save_book(book)
 
     def get_all_books(self):
         return self.books
 
-b = BooksOrganizer('books')
-b.add_book("Encyklopedia Cyckow", 'Kobiety')
+b = BooksOrganizer("books")
+b.add_book(title="Encyklopedia Cyckow", authors=['Kobiety'])
+# print(b.find_book(title='Encyklopedia Cyckow'))
+b.modify_book('title' ,'Encyklopedia Cyckow', 'authors', ['Kobiety, mezczyzni'])
+
+for book in b.get_all_books():
+    print(book)
